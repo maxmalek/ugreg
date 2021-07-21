4595b52b8d4002df661ecb64dc79596411e805be
Malek Maximilian (maximilian.malek) <maximilian.malek@uni-graz.at>
2021-07-21 12:47:24 +0200

    rudimentary signal handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <sstream>
 #include <atomic>
+#include <assert.h>
+#include <signal.h>
 
-#include "variant.h"
 #include "webserver.h"
 #include "config.h"
-
-
-#include <assert.h>
-#include "json_in.h"
-
-#include "civetweb/civetweb.h"
 #include "handler_get.h"
 
 #include <Windows.h> // FIXME: kill this shit
 
+#ifndef SIGQUIT
+#define SIGQUIT 3
+#endif
+
+std::atomic<bool> s_quit;
+
+static void handlesigs(void (*f)(int))
+{
+    if(!f)
+        f = SIG_DFL;
+    signal(SIGBREAK, f);
+    signal(SIGQUIT, f);
+    signal(SIGTERM, f);
+    signal(SIGINT, f);
+}
+
+static void sigquit(int)
+{
+    s_quit = true;
+    handlesigs(NULL);
+}
 
 static int server(const ServerConfig& cfg)
 {
@@ -28,10 +44,10 @@ static int server(const ServerConfig& cfg)
     TreeHandler htree(4); // strlen("/get")
     srv.registerHandler("/get", htree.Handler, &htree);
 
+    while(!s_quit)
+        Sleep(100);
 
-    for(;;)
-        Sleep(1000);
-
+    printf("Stopping server...");
     srv.stop();
 
     WebServer::StaticShutdown();
@@ -40,6 +56,8 @@ static int server(const ServerConfig& cfg)
 
 int main(int argc, char** argv)
 {
+    handlesigs(sigquit);
+
     ServerConfig cfg;
 
     int ret = server(cfg);
