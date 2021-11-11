c8df0d70cda0837e0e945e74580db9d8f5936759
Malek Maximilian (maximilian.malek) <maximilian.malek@uni-graz.at>
2021-11-11 10:20:47 +0100

    simplify fetch config draft, use a .py script to pull from LDAP
    easiest way because:
    - adding openldap as dependency is no-no
    - ldapsearch doesn't really exist on windows
    - ldap3 is a pure-py impl that sucks less than the alternatives

---
+++ bin/ldap.py
@@ -0,0 +1,24 @@
+import ldap3
+import argparse
+import sys
+import json
+from os import environ as env
+
+host = env["HOST"]
+dn = env["DN"]
+filt = env["FILTER"]
+pwd = env["PASS"]
+fields = env["FIELDS"].split()
+base = env["BASE"]
+
+conn = ldap3.Connection(host, user=dn, password=pwd, lazy=False, read_only=True, auto_range=True, auto_bind=True)
+
+if len(sys.argv) > 1 and sys.argv[1] == "--test":
+    exit(0) # an unsuccessful bind throws an error, so once we're here everything is fine
+
+gen = conn.extend.standard.paged_search(base, filt, attributes = fields)
+
+print("[")
+for e in gen:
+    print(json.dumps(dict(e["attributes"])), ",", sep="")
+print("]")
