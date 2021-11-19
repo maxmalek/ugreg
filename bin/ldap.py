577f92898fdd67b4007a221675e2178c8d81717e
Malek Maximilian (maximilian.malek) <maximilian.malek@uni-graz.at>
2021-11-19 18:40:53 +0100

    make ldap python fetcher script work with single entry fetch; cleanups

--- bin/ldap.py
+++ bin/ldap.py
@@ -1,19 +1,47 @@
+#!/usr/bin/env python3
+
 import ldap3
 import argparse
 import sys
 import json
+import re
 from os import environ as env
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 host = env["HOST"]
 dn = env["DN"]
-filt = env["FILTER"]
+filtfmt = env["FILTER"]
 pwd = env["PASS"]
 fields = env["FIELDS"].split()
 base = env["BASE"]
 
-if len(sys.argv) > 1 and sys.argv[1] == "--test":
+#eprint(" ".join(sys.argv))
+
+if len(sys.argv) <= 1:
+    exit(1)
+
+# this is the startup check. Make sure the script can load all dependencies and get here, then we can exit.
+if sys.argv[1] == "--check":
     exit(0)
-    
+
+username = None
+if sys.argv[1] == "--all":
+    username = "*"
+elif sys.argv[1] == "--single":
+    if len(sys.argv) <= 2:
+        exit(2)
+    # special chars reserved by LDAP that must be escaped with \ followed by their hex value
+    rx = re.compile("[" + re.escape("()&|=!><~*/\\") + "]")
+    username = rx.sub(lambda match: "\\" + format(ord(match.group(0)), "x"), sys.argv[2])
+    if len(username) == 0:
+        exit(3)
+
+filt = filtfmt.format(username)
+#eprint(filt)
+
+
 conn = ldap3.Connection(host, user=dn, password=pwd, lazy=False, read_only=True, auto_range=True, auto_bind=True)
 
 # an unsuccessful bind throws an error, so once we're here everything is fine
