maxmalek/ugreg

commit 36106fe0c145217c5614f8f6fa3544982ee52773

maiden: preparations for request relaying

src/maiden/mxhttprequest.cpp
@@ -42,19 +42,45 @@ static void formatHeaders(std::ostringstream& os, const VarCRef& hdrs)
     }
 }
 
-static void formatGet(std::ostringstream& os, const URLTarget& target, const VarCRef& hdrs)
+static void appendList(std::ostringstream& os, bool& first, const char *item)
+{
+    if(first)
+    {
+        os << ", ";
+        first = false;
+    }
+    os << item;
+}
+
+static void formatAccept(std::ostringstream& os, RequestFormat fmt)
+{
+    os << "Accept: ";
+    if(fmt == RQFMT_DEFAULT)
+        os << "*/*";
+    else
+    {
+        bool first = true;
+        if(fmt & RQFMT_JSON)
+            appendList(os, first, "application/json");
+        if(fmt & RQFMT_BJ)
+            appendList(os, first, "application/prs.bj");
+    }
+    os << "\r\n";
+}
+
+static void formatGet(std::ostringstream& os, const URLTarget& target, const VarCRef& hdrs, RequestFormat fmt)
 {
     assert(!target.path.empty());
     assert(!target.host.empty());
     os << "GET " << target.path << " HTTP/1.1\r\n"
         << "Host: " << target.host << "\r\n"
-        << "Connection: close\r\n"
-        << "Accept: application/json\r\n";
+        << "Connection: close\r\n";
+    formatAccept(os, fmt);
     formatHeaders(os, hdrs);
     os << "\r\n";
 }
 
-static void formatPost(std::ostringstream& os, const URLTarget& target, const VarCRef& hdrs)
+static void formatPost(std::ostringstream& os, const URLTarget& target, const VarCRef& hdrs, RequestFormat fmt)
 {
     assert(!target.path.empty());
     assert(!target.host.empty());
@@ -62,8 +88,9 @@ static void formatPost(std::ostringstream& os, const URLTarget& target, const Va
         << "Host: " << target.host << "\r\n"
         << "Connection: close\r\n"
         << "Content-Type: application/json\r\n"
-        << "Accept: application/json\r\n"
         << "Transfer-Encoding: chunked\r\n";
+
+    formatAccept(os, fmt);
     formatHeaders(os, hdrs);
     os << "\r\n";
 }
@@ -77,7 +104,7 @@ static void sendHeaderAndBody(mg_connection *c, const VarCRef& data, const char 
     writeJson(out, data, false);
 }
 
-MxGetJsonResult mxRequestJson(RequestType rqt, VarRef dst, const URLTarget& target, const VarCRef& data, const VarCRef& headers, int timeoutMS, size_t maxsize)
+MxGetJsonResult mxSendRequest(RequestType rqt, VarRef dst, const URLTarget& target, RequestFormat fmt, const VarCRef& data, const VarCRef& headers, int timeoutMS, size_t maxsize)
 {
     char errbuf[1024] = { 0 };
     MxGetJsonResult ret = {MXGJ_CONNECT_FAILED, -1};
@@ -90,10 +117,10 @@ MxGetJsonResult mxRequestJson(RequestType rqt, VarRef dst, const URLTarget& targ
             default: assert(false); [[fallthrough]];
             case RQ_GET:
                 assert(!data);
-                formatGet(os, target, headers);
+                formatGet(os, target, headers, fmt);
                 break;
             case RQ_POST:
-                formatPost(os, target, headers);
+                formatPost(os, target, headers, fmt);
                 break; 
         }
 

src/maiden/handler_mxid_v2.cpp
@@ -320,7 +320,7 @@ int MxidHandler_v2::post_account_register(BufferedWriteStream& dst, mg_connectio
         target.port = resolv.target.port;
         target.ssl = true; // FIXME: autodetect based on port or can we deduce this from something else?
 
-        MxGetJsonResult jr = mxRequestJson(RQ_GET, tmp.root(), target, VarCRef(), VarCRef(), 5000, 4*1024);
+        MxGetJsonResult jr = mxSendRequest(RQ_GET, tmp.root(), target, RQFMT_JSON, VarCRef(), VarCRef(), 5000, 4*1024);
         switch(jr.code)
         {
             case MXGJ_OK:

src/maiden/mxhttprequest.h
@@ -24,4 +24,4 @@ struct MxGetJsonResult
 };
 
 mg_connection* mxConnectTo(const URLTarget& target, char* errbuf, size_t errbufsz);
-MxGetJsonResult mxRequestJson(RequestType rqt, VarRef dst, const URLTarget& target, const VarCRef& data = VarCRef(), const VarCRef& headers = VarCRef(), int timeoutMS = -1, size_t maxsize = 0);
+MxGetJsonResult mxSendRequest(RequestType rqt, VarRef dst, const URLTarget& target, RequestFormat fmt, const VarCRef& data = VarCRef(), const VarCRef& headers = VarCRef(), int timeoutMS = -1, size_t maxsize = 0);

src/maiden/mxresolv.cpp
@@ -156,7 +156,7 @@ MxResolvList lookupHomeserverForHost(const char* host, u64 timeoutMS, size_t max
     target.path = what;
     target.ssl = true;
 
-    MxGetJsonResult jr = mxRequestJson(RQ_GET, tmp.root(),target, VarCRef(), VarCRef(), (int)timeoutMS, maxsize);
+    MxGetJsonResult jr = mxSendRequest(RQ_GET, tmp.root(), target, RQFMT_JSON, VarCRef(), VarCRef(), (int)timeoutMS, maxsize);
     if(jr.code == MXGJ_OK)
     {
         MxResolvResult res;

src/maiden/mxservices.cpp
@@ -127,7 +127,6 @@ static bool initOneServer(MxSearchHandler::ServerConfig& srv, VarCRef x)
     return true;
 }
 
-
 inline static const char *yesno(bool x) { return x ? "yes" : "no"; }
 bool MxSearchHandler::init(VarCRef cfg)
 {
@@ -380,6 +379,12 @@ void MxSearchHandler::_ApplyElementHack(MxSearchResults& results, const std::str
     }
 }
 
+const MxSearchHandler::AccessKeyConfig* MxSearchHandler::checkAccessKey(const std::string& token) const
+{
+    AccessKeyMap::const_iterator it = accessKeys.find(token);
+    return it != accessKeys.end() ? &it->second : NULL;
+}
+
 MxSearchHandler::MxSearchResultsEx MxSearchHandler::doSearch(const char* term, size_t limit, VarCRef hsResultsArray) const
 {
     const std::vector<TwoWayCasefoldMatcher> matchers = mxBuildMatchersForTerm(term);
@@ -506,7 +511,25 @@ int MxSearchHandler::onRequest(BufferedWriteStream& dst, mg_connection* conn, co
                 DataTree hsdata(DataTree::TINY);
                 bool useHS = false;
                 VarCRef xhsResultsArray;
-                if(askHS)
+                bool raw = false;
+                bool forwardRequest = askHS;
+
+                if(const AccessKeyConfig *acfg = checkAccessKey(rq.authorization))
+                {
+                    // We're in relay mode -- don't go to the HS.
+                    // Someone with special authorization is asking, so we just provide
+                    // from our own cache, without massaging the results in any way.
+                    if(acfg->enabled)
+                    {
+                        raw = true;
+                        forwardRequest = false;
+                    }
+                    // else we send the known-bad key to the HS, that's going to refuse it as usual,
+                    // and an eventual attacker will be none the wiser.
+                    // ie. we don't send a specialized error message about a disabled key on purpose.
+                }
+
+                if(forwardRequest)
                 {
                     bool askThisTime = true;
 
@@ -532,7 +555,7 @@ int MxSearchHandler::onRequest(BufferedWriteStream& dst, mg_connection* conn, co
                         URLTarget hs = this->homeserver.target;
                         hs.path = ClientPrefix + rq.query; // forward URL as-is
                         ScopeTimer tm;
-                        MxGetJsonResult jr = mxRequestJson(RQ_POST, hsdata.root(), hs, vars.root(), headers, this->homeserver.timeout);
+                        MxGetJsonResult jr = mxSendRequest(RQ_POST, hsdata.root(), hs, RQFMT_JSON | RQFMT_BJ, vars.root(), headers, this->homeserver.timeout);
                         logdev("mxRequestJson done after %u ms, result = %u", (unsigned)tm.ms(), jr.code);
                         headers.clear();
                         useHS = jr.code == MXGJ_OK && !hsdata.root().lookup("error");
@@ -565,15 +588,23 @@ int MxSearchHandler::onRequest(BufferedWriteStream& dst, mg_connection* conn, co
                     }
                 }
 
+                assert(!term.empty());
+
                 MxSearchResultsEx rx = doSearch(term.c_str(), limit, xhsResultsArray);
 
-                if(searchcfg.element_hack)
+                if(!raw && searchcfg.element_hack)
                     _ApplyElementHack(rx.results, term);
 
                 // re-use vars for the search since we don't need those anymore
                 translateResults(vars.root(), rx);
 
-                writeJson(dst, vars.root(), false);
+                // send in json, unless the requesting client supports BJ (relay mode)
+                serialize::Format fmt = serialize::JSON;
+                if(rq.fmt & RQFMT_BJ)
+                    fmt = serialize::BJ;
+
+                serialize::save(dst, vars.root(), fmt);
+
                 return 0;
             }
         }

src/maiden/mxservices.h
@@ -65,6 +65,7 @@ private:
     void translateResults(VarRef dst, const MxSearchResultsEx& results) const;
     MxSearchResults mergeResults(const MxSearchResults& myresults, const MxSearchResults& hsresults) const;
     static void _ApplyElementHack(MxSearchResults& results, const std::string& term);
+    const AccessKeyConfig *checkAccessKey(const std::string& token) const;
 
     MxSources& _sources;
 };

src/server/request.cpp
@@ -46,6 +46,33 @@ static CompressionType parseEncoding(const char *enc)
     return c;
 }
 
+static const bool inlist(const char *list, const char *item)
+{
+    const char *p = list;
+    for(;;)
+    {
+        p = strstr(p, item);
+        if(!p)
+            return false;
+        const char next = p[strlen(item)];
+        if(!next || next == ',' || next == ';')
+            return true;
+    }
+}
+
+// Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8
+static RequestFormat parseAccept(const char *enc)
+{
+    RequestFormat f = RQFMT_DEFAULT;
+
+    if(inlist(enc, "application/json"))
+        f |= RQFMT_JSON;
+    if(inlist(enc, "application/prs.bj")) // I'm just making this up. (See RFC 2048, RFC 6838)
+        f |= RQFMT_BJ;
+
+    return f;
+}
+
 static bool parseAndApplyVars(Request& r, const char *vars)
 {
     char tmp[8 * 1024];
@@ -72,6 +99,8 @@ static bool parseAndApplyVars(Request& r, const char *vars)
         }
         else if(!strcmp(hd[i].name, "json"))
             r.fmt = RQFMT_JSON;
+        else if(!strcmp(hd[i].name, "bj"))
+            r.fmt = RQFMT_BJ;
     }
     return true;
 }
@@ -145,6 +174,10 @@ bool Request::parse(const mg_request_info* info, size_t skipFromQuery)
         if (!mg_strcasecmp("Authorization", k))
         {
             authorization = v;
+        }
+        if (!mg_strcasecmp("Accept", k)) // which MIME type the client prefers to see
+        {
+            fmt |= parseAccept(v);
         }
     }
 

src/server/request.h
@@ -51,11 +51,14 @@ inline static RequestFlags operator|(RequestFlags a, RequestFlags b) { return Re
 inline static RequestFlags& operator|=(RequestFlags& a, RequestFlags b) { return (a = RequestFlags(unsigned(a) | unsigned(b))); }
 
 
-enum RequestFormat
+enum RequestFormat // bitmask
 {
     RQFMT_DEFAULT = 0,
-    RQFMT_JSON,
+    RQFMT_JSON    = 0x01,
+    RQFMT_BJ      = 0x02,
 };
+inline static RequestFormat operator|(RequestFormat a, RequestFormat b) { return RequestFormat(unsigned(a) | unsigned(b)); }
+inline static RequestFormat& operator|=(RequestFormat& a, RequestFormat b) { return (a = RequestFormat(unsigned(a) | unsigned(b))); }
 
 static const char* RequestFormatName[] =
 {

commit 06336290d68efea2c9ec2fab1b8f0b35806ea82d

maiden: fix typo that would break most outgoing http requests
Broke this in 36106fe0c145217c56

src/maiden/mxhttprequest.cpp
@@ -44,11 +44,9 @@ static void formatHeaders(std::ostringstream& os, const VarCRef& hdrs)
 
 static void appendList(std::ostringstream& os, bool& first, const char *item)
 {
-    if(first)
-    {
+    if(!first)
         os << ", ";
-        first = false;
-    }
+    first = false;
     os << item;
 }
 